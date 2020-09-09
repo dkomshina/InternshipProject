d4d627a86e22d098f66917f042662f0857fe871d
Daria Komshina <dkomshina@mail.ru>
2020-09-09 10:09:42 +0300

    Initial commit (2)

---
+++ src/test/java/sberbank/internship/dkomshina/InternshipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sberbank.internship.dkomshina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
